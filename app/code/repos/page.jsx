6b7ca036f38c04af4f2236a6dc1d893903d01fd6
nathaniel <yang47984@gmail.com>
2023-10-25 02:28:52 +0800

    Data Fetching

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,5 +1,17 @@
-const ReposPage = () => {
-	return <div>ReposPage</div>;
+async function fetchRepos() {
+	// 1.使用 fetch api 获取数据
+	const response = await fetch(
+		'https://api.github.com/users/bradtraversy/repos'
+	);
+	// 2.解析为 json 格式
+	const repos = await response.json();
+	return repos;
+}
+
+const ReposPage = async () => {
+	const repos = await fetchRepos();
+	console.log(repos);
+	return <div>{repos[0].name}</div>;
 };
 
 export default ReposPage;
