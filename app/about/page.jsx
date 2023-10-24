ef89b656e22ecc36085af7c1b44b20b2026970f4
nathaniel <yang47984@gmail.com>
2023-10-24 22:26:54 +0800

    Nested Routes

---
+++ app/about/page.jsx
@@ -0,0 +1,15 @@
+const AboutPage = () => {
+	return (
+		<div>
+			<h1>About Traversy Media</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Accusamus, enim provident sed nisi quis quaerat? Soluta, minus?
+				Sunt dolores cumque, quo ab distinctio tempora totam, doloremque
+				natus, minima debitis laborum.
+			</p>
+		</div>
+	);
+};
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+const TeamPage = () => {
+	return (
+		<div>
+			<h1>Our Team</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Corrupti, ratione sed. Officia illum, deleniti inventore facere
+				adipisci repellendus esse voluptatibus sed facilis omnis
+				quisquam nihil delectus molestiae numquam et labore.
+			</p>
+		</div>
+	);
+};
+
+export default TeamPage;
