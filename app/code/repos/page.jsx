df773368c75ff52b1e81912d05527cb0a169988f
nathaniel <yang47984@gmail.com>
2023-10-25 11:04:33 +0800

    ReposPage Output

--- app/code/repos/page.jsx
+++ app/code/repos/page.jsx
@@ -1,3 +1,7 @@
+import Link from 'next/link';
+
+import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
+
 async function fetchRepos() {
 	// 1.使用 fetch api 获取数据
 	const response = await fetch(
@@ -10,8 +14,32 @@ async function fetchRepos() {
 
 const ReposPage = async () => {
 	const repos = await fetchRepos();
-	console.log(repos);
-	return <div>{repos[0].name}</div>;
+	return (
+		<div className='repos-container'>
+			<h2>Repositories</h2>
+			<ul className='repo-list'>
+				{repos.map((repo) => (
+					<li key={repo.id}>
+						<Link href={`/code/repos/${repo.name}`}>
+							<h3>{repo.name}</h3>
+							<p>{repo.description}</p>
+							<div className='repo-details'>
+								<span>
+									<FaStar /> {repo.stargazers_count}
+								</span>
+								<span>
+									<FaCodeBranch /> {repo.forks_count}
+								</span>
+								<span>
+									<FaEye /> {repo.watchers_count}
+								</span>
+							</div>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 };
 
 export default ReposPage;
